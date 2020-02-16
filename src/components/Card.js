e1a8f590404a2bfb661af1161306227202cf0624
Patrick Lombardo <patricklombardo@Patricks-MacBook-Pro.local>
2020-02-15 22:52:00 -0500

    build out card component and styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React from 'react';
 
-export const Card = () => {
+export const Card = ({ country }) => {
+  const { name, population, region, capital, flag } = country;
   return (
     <div className="card">
-      <div className="card__image">
-        <span>Image</span>
+      <div className="card__flag">
+        <img src={flag} />
       </div>
-
-      <div className="card__content">
-        <span>Content</span>
+      <div className="card__description">
+        <h2>{name}</h2>
+        <p>
+          <span>Population:</span>
+          {population}
+        </p>
+        <p>
+          <span>Region:</span>
+          {region}
+        </p>
+        <p>
+          <span>Capital:</span>
+          {capital}
+        </p>
       </div>
     </div>
   );
 };
+
+export default Card;
--- src/containers/CountryList.js
+++ src/containers/CountryList.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
+import Card from '../components/Card';
 
 export const CountryList = ({countries }) => {
+  const testCountry = countries[0];
+  console.log(testCountry);
+
   return (
     <div>
       <Navbar />
+      <Card country={testCountry} />
+
     </div>
   )
 }
